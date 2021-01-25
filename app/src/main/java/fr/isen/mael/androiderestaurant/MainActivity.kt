aa2d056bc995ae520943e3cf95bd21f58e914ecf
Wiifou <mael.bouchand@isen.yncrea.fr>
2021-01-25 17:10:58 +0100

    link button to other pages

--- app/src/main/java/fr/isen/mael/androiderestaurant/MainActivity.kt
+++ app/src/main/java/fr/isen/mael/androiderestaurant/MainActivity.kt
@@ -1,11 +1,32 @@
 package fr.isen.mael.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun callButton(view: View?){
+        val mytoast = Toast.makeText(this.applicationContext,"@entrées",Toast.LENGTH_SHORT)
+        mytoast.show()
+        startActivity(
+            Intent(applicationContext, Entrees_Activity::class.java)
+        )
+    }
+    fun callButton2(view: View?){
+        val mytoast = Toast.makeText(this.applicationContext,"@plats",Toast.LENGTH_SHORT)
+        mytoast.show()
+    }
+    fun callButton3(view: View?){
+        val mytoast = Toast.makeText(this.applicationContext,"@desserts",Toast.LENGTH_SHORT)
+        mytoast.show()
+    }
+
+
 }
