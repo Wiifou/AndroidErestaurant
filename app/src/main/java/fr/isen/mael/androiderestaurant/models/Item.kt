3218c6c66e0bc6b5133132921bcb31b057945298
Wiifou <mael.bouchand@isen.yncrea.fr>
2021-02-10 09:58:02 +0100

    remise a niveau des layouts

--- app/src/main/java/fr/isen/mael/androiderestaurant/models/Item.kt
+++
@@ -1,9 +0,0 @@
-package fr.isen.mael.androiderestaurant.models
-
-import java.io.Serializable
-
-data class Item(
-        var items: List<Plats>,
-        val name_fr: String,
-        val name_en: String
-):Serializable
